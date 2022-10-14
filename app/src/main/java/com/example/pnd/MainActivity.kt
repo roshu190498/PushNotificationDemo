f7ed03b9d41fb36781c99db37524391a56bf8868
testingvalidationola3 <roshu190498@gmail.com>
2022-10-14 10:52:09 +0530

    add files

--- app/src/main/java/com/example/pnd/MainActivity.kt
+++ app/src/main/java/com/example/pnd/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.pnd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
